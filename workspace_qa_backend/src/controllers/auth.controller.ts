bb57a45732414a825c786bf2f90525b322539f9b
atlowChemi <67366097+atlowChemi@users.noreply.github.com>
2020-10-14 21:20:06 +0300

    Add login route and use new exceptions

--- workspace_qa_backend/src/controllers/auth.controller.ts
+++ workspace_qa_backend/src/controllers/auth.controller.ts
@@ -1,5 +1,12 @@
+import { sign } from 'jsonwebtoken';
+
 import { HTTPStatuses } from '../constants';
-import { HttpException } from '../exceptions';
+import {
+    BadRequestException,
+    HttpException,
+    UserExistsException,
+    UserNotFoundException,
+} from '../exceptions';
 import { userModel } from '../models/mongo';
 import { appLogger, createUserFromRequest, isValidUser } from '../services';
 import { IConroller } from '.';
@@ -14,23 +21,17 @@ export const authController: IConroller = {
                 // we can find by username or email because they are unique
                 const { email, username } = req.body;
                 try {
-                    let user = await userModel.findOne({ email });
-                    if (user) {
-                        throw new HttpException(
-                            HTTPStatuses.unauthorized,
-                            'User with this email already exists!'
-                        );
+                    const userByEmail = await userModel.findOne({ email });
+                    if (userByEmail) {
+                        throw new UserExistsException('email');
                     }
-                    user = await userModel.findOne({ username });
-                    if (user) {
-                        throw new HttpException(
-                            HTTPStatuses.unauthorized,
-                            'User with this username already exists!'
-                        );
+                    const userByName = await userModel.findOne({ username });
+                    if (userByName) {
+                        throw new UserExistsException('username');
                     }
                     const newUser = createUserFromRequest(req);
                     await newUser?.save();
-                    res.status(HTTPStatuses.ok).send({
+                    res.send({
                         success: true,
                         user: userModel,
                     });
@@ -46,7 +47,44 @@ export const authController: IConroller = {
                 }
             } else {
                 appLogger.error('Did not get data for register!');
-                throw new HttpException(HTTPStatuses.clientError, 'Some user data is lacking!');
+                throw new BadRequestException('Some user data is lacking!');
+            }
+        },
+    },
+    login: {
+        path: '/login',
+        method: 'post',
+        controller: async (req, res) => {
+            const { email, password } = req.body;
+            if (!email || !password) {
+                throw new BadRequestException('Login data not passed!');
+            }
+            try {
+                const user = await userModel.findOne({ email });
+                if (!user) {
+                    throw new UserNotFoundException(email);
+                }
+                const isMatch = await user.comparePassword(password);
+                if (!isMatch) {
+                    throw new HttpException(
+                        HTTPStatuses.unauthorized,
+                        'This is the wrong password!'
+                    );
+                }
+                //If we are here, user exists and this is correct password
+                const { secret = '', expire = 18000000 } = process.env;
+                const token = sign(user.toJSON(), secret, { expiresIn: expire });
+                res.send({
+                    success: true,
+                    user: user,
+                    token: `JWT ${token}`,
+                });
+            } catch (error) {
+                if (error instanceof HttpException) {
+                    throw error;
+                }
+                appLogger.error('could not login', error);
+                throw new HttpException(HTTPStatuses.internalServerError, 'Could not login user!');
             }
         },
     },
