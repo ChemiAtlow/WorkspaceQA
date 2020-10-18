f395d4affa73d0fe0bad21c0aa23920321751ee0
atlowChemi <67366097+atlowChemi@users.noreply.github.com>
2020-10-18 10:00:01 +0300

    asyncify and add edit option

--- workspace_qa_backend/src/middleware/validation.middleware.ts
+++ workspace_qa_backend/src/middleware/validation.middleware.ts
@@ -1,20 +1,21 @@
 import { plainToClass } from 'class-transformer';
-import { validate, ValidationError } from 'class-validator';
+import { validate } from 'class-validator';
 import { RequestHandler } from 'express';
 import { HTTPStatuses } from '../constants';
 import { HttpException } from '../exceptions';
 
-export function validationMiddleware<T>(type: { new (): T }): RequestHandler {
-    return (req, _, next) => {
-        validate(plainToClass(type, req.body)).then((errors: ValidationError[]) => {
-            if (errors.length > 0) {
-                const message = errors
-                    .map((error: ValidationError) => Object.values(error.constraints || []))
-                    .join(', ');
-                next(new HttpException(HTTPStatuses.clientError, message));
-            } else {
-                next();
-            }
-        });
+export function validationMiddleware<T>(
+    type: { new (): T },
+    skipMissingProperties = false
+): RequestHandler {
+    return async (req, _, next) => {
+        const errors = await validate(plainToClass(type, req.body), { skipMissingProperties });
+        if (errors.length > 0) {
+            const message = errors
+                .map((error) => Object.values(error.constraints || []))
+                .join(', ');
+            return next(new HttpException(HTTPStatuses.clientError, message));
+        }
+        return next();
     };
 }
