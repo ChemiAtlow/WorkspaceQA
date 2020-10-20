5cb35afba28b2b6d8c14f308770cf615f3cc5564
atlowChemi <67366097+atlowChemi@users.noreply.github.com>
2020-10-20 12:12:51 +0300

    Add endpoint for creating project

--- workspace_qa_backend/src/controllers/projects.controller.ts
+++ workspace_qa_backend/src/controllers/projects.controller.ts
@@ -1,6 +1,8 @@
 import { IConroller } from '.';
 import { InternalServerException } from '../exceptions';
-import { userModel } from '../models/DB/schemas';
+import { validationMiddleware } from '../middleware';
+import { ProjectDto } from '../models/DB/dtos';
+import { userModel, projectModel } from '../models/DB/schemas';
 
 export const projectsControllers: IConroller = {
     getUsersProject: {
@@ -13,8 +15,37 @@ export const projectsControllers: IConroller = {
                 throw new InternalServerException('An error happened with authentication');
             }
             console.log(user.projects);
-            const userProjects = await userModel.findById(user._id).populate('projects').exec();
-            res.send(userProjects?.projects);
+            const userProjects = await userModel
+                .findById(user._id, 'name username')
+                .populate('projects', 'name users.role')
+                .exec();
+            res.send(userProjects);
+        },
+    },
+    create: {
+        path: '/',
+        method: 'post',
+        authSafe: true,
+        middleware: [validationMiddleware(ProjectDto)],
+        controller: async (req, res) => {
+            const { body, user } = req;
+            if (user === undefined) {
+                throw new InternalServerException('An error happened with authentication');
+            }
+            const { name, avatar, _id: id } = user;
+            const projectData: ProjectDto = body;
+            try {
+                const project = await projectModel.create({
+                    ...projectData,
+                    questions: [],
+                    users: [{ name, avatar, id, role: 'Owner' }],
+                });
+                user.projects.push(project);
+                await user.save();
+                res.send({ ...project });
+            } catch (error) {
+                throw new InternalServerException('Issue creating project!');
+            }
         },
     },
 };
--- workspace_qa_backend/src/models/DB/schemas/index.ts
+++ workspace_qa_backend/src/models/DB/schemas/index.ts
@@ -1,1 +1,2 @@
 export { userModel } from './user.model';
+export { projectModel } from './project.model';
