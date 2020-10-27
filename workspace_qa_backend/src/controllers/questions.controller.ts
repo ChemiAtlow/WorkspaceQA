3f4cadac9d317c75550f1f70f801bec36fc8b8b8
atlowChemi <67366097+atlowChemi@users.noreply.github.com>
2020-10-27 10:14:14 +0200

    Finish refactoring after Elad Scalability

--- workspace_qa_backend/src/controllers/questions.controller.ts
+++ workspace_qa_backend/src/controllers/questions.controller.ts
@@ -1,40 +1,17 @@
 import { IConroller } from '.';
-import { HttpException, InternalServerException, UnauthorizedException } from '../exceptions';
+import { HTTPStatuses } from '../constants';
+import {
+    HttpException,
+    InternalServerException,
+    QuestionNotFoundException,
+    UnauthorizedException,
+} from '../exceptions';
 import { validationMiddleware } from '../middleware';
 import { projectModel, questionModel } from '../models/DB/schemas';
 import { CreateQuestionDto } from '../models/dtos';
 import { appLogger, getSocketIO } from '../services';
 
 export const questionsController: IConroller = {
-    getQuestions: {
-        path: '/',
-        method: 'get',
-        authSafe: true,
-        controller: async (req, res) => {
-            const { user, projectId } = req;
-            if (user === undefined) {
-                throw new InternalServerException('An error happened with authentication');
-            }
-            try {
-                const project = await projectModel
-                    .findOne({
-                        _id: projectId,
-                        users: { $elemMatch: { id: { $eq: user.id }, role: { $ne: 'Removed' } } },
-                    })
-                    .select('-users -archived -name');
-                if (!project) {
-                    throw new UnauthorizedException('You are not a part of this project!');
-                }
-                res.send(project);
-            } catch (error) {
-                if (error instanceof HttpException) {
-                    throw error;
-                }
-                appLogger.error(error.message);
-                throw new InternalServerException('Issue getting question');
-            }
-        },
-    },
     createQuestion: {
         path: '/',
         method: 'post',
@@ -45,25 +22,23 @@ export const questionsController: IConroller = {
             if (user === undefined) {
                 throw new InternalServerException('An error happened with authentication');
             }
+            const isUserFromProject = user.projects.some((prj) => prj._id.equals(projectId));
+            if (!isUserFromProject) {
+                throw new UnauthorizedException('You are not a part of this project!');
+            }
             const { name, avatar, _id } = user;
-            const questionData: CreateQuestionDto = body;
+            const { title, filePath, message } = body as CreateQuestionDto;
             try {
-                const project = await projectModel.findOne({
-                    _id: projectId,
-                    users: { $elemMatch: { id: { $eq: user.id }, role: { $ne: 'Removed' } } },
-                });
-                if (!project) {
-                    throw new UnauthorizedException(
-                        'You are not authorized to ask in this project!'
-                    );
-                }
                 const question = new questionModel({
-                    title: questionData.title,
-                    filePath: questionData.filePath,
-                    project,
+                    title,
+                    filePath,
+                    project: projectId,
                     question: {
-                        message: questionData.message,
+                        message,
                         revisions: [],
+                        ratings: {
+                            votes: [],
+                        },
                         user: {
                             _id,
                             name,
@@ -72,26 +47,69 @@ export const questionsController: IConroller = {
                     },
                     answers: [],
                 });
-                project.questions.push(question);
-                await Promise.all([question.save(), project.save()]);
-                project.users.forEach((usr) => {
-                    getSocketIO()
-                        .sockets.to(`user${usr.id}`)
-                        .emit('questions', {
-                            action: 'create',
-                            question: {
-                                ...question.toObject(),
-                            },
-                        });
-                });
-                res.status(201).send(question);
+                await question.save();
+                getSocketIO()
+                    .sockets.to(`project${projectId}`)
+                    .emit('questions', {
+                        action: 'create',
+                        question: {
+                            _id: question._id,
+                            title,
+                            project: projectId,
+                            state: question.state,
+                            answerCount: question.answerCount,
+                        },
+                    });
+                res.status(201).send({ question });
             } catch (err) {
                 appLogger.error(err.message);
+                throw new InternalServerException('Issue creating question!');
+            }
+        },
+    },
+    getAQuestion: {
+        path: '/:questionId',
+        method: 'get',
+        authSafe: true,
+        controller: async (req, res) => {
+            const { user, projectId, params } = req;
+            if (user === undefined) {
+                throw new InternalServerException('An error happened with authentication');
+            }
+            const isUserFromProject = user.projects.some((prj) => prj._id.equals(projectId));
+            if (!isUserFromProject) {
+                throw new UnauthorizedException('You are not a part of this project!');
+            }
+            const { questionId } = params;
+            try {
+                const question = await questionModel.findById(questionId);
+                if (!question) {
+                    throw new QuestionNotFoundException(questionId);
+                }
+                res.send(question);
+            } catch (err) {
+                appLogger.error(err);
                 if (err instanceof HttpException) {
                     throw err;
                 }
-                throw new InternalServerException('Issue creating the question!');
+                throw new InternalServerException('Issue getting question!');
             }
+        },
+    },
+    editQuestion: {
+        path: '/:questionId',
+        method: 'patch',
+        authSafe: true,
+        controller: (req, res) => {
+            throw new HttpException(HTTPStatuses.notImplemented, 'Route not implemented!');
+        },
+    },
+    addAnswer: {
+        path: '/:questionId',
+        method: 'patch',
+        authSafe: true,
+        controller: (req, res) => {
+            throw new HttpException(HTTPStatuses.notImplemented, 'Route not implemented!');
         },
     },
 };
