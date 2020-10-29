944cff3593ddfe54fbf1557d1cdea0cacb918d4a
ChemiAtlow <17195175+ChemiAtlow@users.noreply.github.com>
2020-10-29 13:30:46 +0200

    Migrate to servic + more endpoints

--- workspace_qa_backend/src/controllers/questions.controller.ts
+++ workspace_qa_backend/src/controllers/questions.controller.ts
@@ -1,15 +1,23 @@
-import { IConroller } from '.';
 import { HTTPStatuses } from '../constants';
-import {
-    HttpException,
-    InternalServerException,
-    QuestionNotFoundException,
-    UnauthorizedException,
-} from '../exceptions';
+import { HttpException, InternalServerException, UnauthorizedException } from '../exceptions';
 import { validationMiddleware } from '../middleware';
-import { questionModel, responseModel } from '../models/DB/schemas';
-import { CreateQuestionDto } from '../models/dtos';
-import { appLogger, emitQuestionCreated, emitQuestionEditedAtProjectLevel } from '../services';
+import { CreateQuestionDto, ResponseDto } from '../models/dtos';
+import { IConroller } from '../models/interfaces';
+import {
+    addAnswerToQuestion,
+    appLogger,
+    createQuestion,
+    createResponse,
+    emitQuestionCreated,
+    emitQuestionEditedAtProjectLevel,
+    getAnswer,
+    getAnswers,
+    getQuestion,
+    isUserFromProject,
+    isUserResponseOwner,
+    updateQuestion,
+    updateResponse,
+} from '../services';
 
 export const questionsController: IConroller = {
     createQuestion: {
@@ -22,36 +30,16 @@ export const questionsController: IConroller = {
             if (user === undefined) {
                 throw new InternalServerException('An error happened with authentication');
             }
-            const isUserFromProject = user.projects.some((prj) => prj._id.equals(projectId));
-            if (!isUserFromProject) {
+            if (!isUserFromProject(user, projectId)) {
                 throw new UnauthorizedException('You are not a part of this project!');
             }
-            const { name, avatar, _id } = user;
-            const { title, filePath, message } = body as CreateQuestionDto;
             try {
-                const questionResponse = new responseModel({
-                    message,
-                    revisions: [],
-                    ratings: {
-                        votes: [],
-                    },
-                    user: {
-                        _id,
-                        name,
-                        avatar,
-                    },
-                });
-                const question = new questionModel({
-                    title,
-                    filePath,
-                    project: projectId,
-                    question: questionResponse,
-                    answers: [],
-                });
+                const questionResponse = createResponse(body, user);
+                const question = createQuestion(projectId, body, questionResponse);
                 await Promise.all([questionResponse.save(), question.save()]);
                 emitQuestionCreated(projectId, {
                     _id: question._id,
-                    title: title || '',
+                    title: question.title,
                     state: question.state,
                     answerCount: question.answerCount,
                 });
@@ -67,20 +55,19 @@ export const questionsController: IConroller = {
         method: 'get',
         authSafe: true,
         controller: async (req, res) => {
-            const { user, projectId, params } = req;
+            const {
+                user,
+                projectId,
+                params: { questionId },
+            } = req;
             if (user === undefined) {
                 throw new InternalServerException('An error happened with authentication');
             }
-            const isUserFromProject = user.projects.some((prj) => prj._id.equals(projectId));
-            if (!isUserFromProject) {
+            if (!isUserFromProject(user, projectId)) {
                 throw new UnauthorizedException('You are not a part of this project!');
             }
-            const { questionId } = params;
             try {
-                const question = await questionModel.findById(questionId).populate('question');
-                if (!question) {
-                    throw new QuestionNotFoundException(questionId);
-                }
+                const question = await getQuestion(questionId);
                 res.send(question);
             } catch (err) {
                 appLogger.error(err.message);
@@ -97,37 +84,30 @@ export const questionsController: IConroller = {
         authSafe: true,
         middleware: [validationMiddleware(CreateQuestionDto, true)],
         controller: async (req, res) => {
-            const { user, projectId, params, body } = req;
+            const {
+                user,
+                projectId,
+                params: { questionId },
+                body,
+            } = req;
             if (user === undefined) {
                 throw new InternalServerException('An error happened with authentication');
             }
-            const isUserFromProject = user.projects.some((prj) => prj._id.equals(projectId));
-            if (!isUserFromProject) {
+            if (!isUserFromProject(user, projectId)) {
                 throw new UnauthorizedException('You are not a part of this project!');
             }
-            const { questionId } = params;
             try {
-                const questionDoc = await questionModel.findById(questionId).populate('question');
-                if (!questionDoc) {
-                    throw new QuestionNotFoundException(questionId);
-                }
-                const isQuestionOwner = questionDoc.question.user._id.equals(user._id);
-                if (!isQuestionOwner) {
+                const questionDoc = await getQuestion(questionId);
+                if (!isUserResponseOwner(questionDoc.question, user)) {
                     throw new UnauthorizedException('You did not write this question!');
                 }
-                const { filePath, message, title: newTitle } = body as CreateQuestionDto;
-                if (message) {
-                    questionDoc.question.message = message;
-                    await responseModel
-                        .updateOne({ _id: questionDoc.question }, { message })
-                        .exec();
+                await updateQuestion(body, questionDoc);
+                const { filePath, message, title } = body as CreateQuestionDto;
+                if (message || filePath) {
+                    //TODO: Update at question level
                 }
-                if (newTitle || filePath) {
-                    questionDoc.title = newTitle || questionDoc.title;
-                    questionDoc.filePath = filePath || questionDoc.filePath;
-                    await questionDoc.updateOne({ ...questionDoc.toObject() }).exec();
-                    const { _id, state, answerCount, title } = questionDoc;
-                    emitQuestionEditedAtProjectLevel(projectId, { title, _id, answerCount, state });
+                if (title) {
+                    emitQuestionEditedAtProjectLevel(projectId, questionDoc);
                 }
                 res.send(questionDoc);
             } catch (err) {
@@ -139,20 +119,87 @@ export const questionsController: IConroller = {
             }
         },
     },
+    getAnswers: {
+        path: '/:questionId/answers',
+        method: 'get',
+        authSafe: true,
+        controller: async (req, res) => {
+            const {
+                user,
+                projectId,
+                params: { questionId },
+            } = req;
+            if (user === undefined) {
+                throw new InternalServerException('An error happened with authentication');
+            }
+            if (!isUserFromProject(user, projectId)) {
+                throw new UnauthorizedException('You are not a part of this project!');
+            }
+            const answers = await getAnswers(questionId);
+            res.send(answers);
+        },
+    },
     addAnswer: {
         path: '/:questionId/answers',
         method: 'post',
         authSafe: true,
-        controller: (req, res) => {
-            throw new HttpException(HTTPStatuses.notImplemented, 'Route not implemented!');
+        middleware: [validationMiddleware(ResponseDto)],
+        controller: async (req, res) => {
+            const {
+                body,
+                user,
+                projectId,
+                params: { questionId },
+            } = req;
+            if (user === undefined) {
+                throw new InternalServerException('An error happened with authentication');
+            }
+            if (!isUserFromProject(user, projectId)) {
+                throw new UnauthorizedException('You are not a part of this project!');
+            }
+            try {
+                const answer = createResponse(body, user);
+                await addAnswerToQuestion(questionId, answer);
+                //TODO: Emit data at question level, and at project level (answeCount)
+                res.send(answer);
+            } catch (err) {
+                appLogger.error(err.message);
+                if (err instanceof HttpException) {
+                    throw err;
+                }
+                throw new InternalServerException('Unknown issue when creating answer!');
+            }
         },
     },
     editAnswer: {
         path: '/:questionId/answers/:answerId',
         method: 'patch',
         authSafe: true,
-        controller: (req, res) => {
-            throw new HttpException(HTTPStatuses.notImplemented, 'Route not implemented!');
+        middleware: [validationMiddleware(ResponseDto)],
+        controller: async (req, res) => {
+            const {
+                body,
+                user,
+                projectId,
+                params: { questionId, answerId },
+            } = req;
+            if (user === undefined) {
+                throw new InternalServerException('An error happened with authentication');
+            }
+            if (!isUserFromProject(user, projectId)) {
+                throw new UnauthorizedException('You are not a part of this project!');
+            }
+            try {
+                const answer = await getAnswer(answerId);
+                const edited = await updateResponse(answer, body);
+                res.send(edited);
+            } catch (err) {
+                appLogger.error(err.message);
+                if (err instanceof HttpException) {
+                    throw err;
+                }
+                throw new InternalServerException('Unknown issue when editing answer!');
+            }
         },
     },
     rateResponse: {
